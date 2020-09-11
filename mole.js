4b49da704bbb407f54f74c1ec3f959af676c96ec
Safiya Cain <say.cain3@gmail.com>
2020-09-11 13:44:43 -0400

    step 3

--- mole.js
+++ mole.js
@@ -12,18 +12,23 @@ function popUpRandomMole() {
     const moleHeads = document.querySelectorAll('.wgs__mole-head');
     let randNum = Math.floor((Math.random() * 7));
     let mole = moleHeads[randNum];
-    mole.classList.add("wgs__mole-head--hidden");
+    mole.classList.remove("wgs__mole-head--hidden");
     setTimeout(() => hideMole(mole), 3000);
 }
 
 function hideMole(mole){
-  mole.classList.remove("wgs__mole-head--hidden");
+  mole.classList.add("wgs__mole-head--hidden");
   setTimeout(popUpRandomMole, 1000);
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    event.stopPropagation();
-    setTimeout(popUpRandomMole, 0);
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(()=> {
+        let moles = document.querySelectorAll(".wgs__mole-head");
+        moles.forEach((mole) =>{
+            mole.classList.add("wgs__mole-head--hidden")
+        })
+        setTimeout(popUpRandomMole, 0);
+    }, 3000);
     const moleHeads = document.querySelector('.pf');
     moleHeads.addEventListener('click', (event) => {
         event.target.classList.add('wgs__mole-head--hidden');
