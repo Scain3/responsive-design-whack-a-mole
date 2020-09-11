adb1d4c1d7f3c538e646769d931d34da7ad8c60b
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-09-11 09:54:29 -0700

    created playing field

--- mole.js
+++ mole.js
@@ -1,9 +1,14 @@
 // Write your JS here.
-window.addEventListener("DOMContentLoaded",() => {
-  setInterval(()=> {
-      const moleHeads = document.querySelectorAll(".wgs__mole-head");
-      for(let moleHead of moleHeads){
-        moleHead.classList.toggle("wgs__mole-head--hidden")
-      }
-  }, 1000)
-})
+// window.addEventListener("DOMContentLoaded",() => {
+//   setInterval(()=> {
+//       const moleHeads = document.querySelectorAll(".wgs__mole-head");
+//       for(let moleHead of moleHeads){
+//         moleHead.classList.toggle("wgs__mole-head--hidden")
+//       }
+//   }, 1000)
+// })
+
+function popUpRandomMole() {
+    const moleHeads = document.querySelectorAll('.wgs__mole-head');
+    let randNum = Math.floor((Math.random() * 7));
+}
