b90e6fe5f9825a6c17847648e48533c6b959ebdf
Safiya Cain <say.cain3@gmail.com>
2020-09-11 13:10:45 -0400

    create popUpRandomMole function

--- mole.js
+++ mole.js
@@ -11,4 +11,16 @@
 function popUpRandomMole() {
     const moleHeads = document.querySelectorAll('.wgs__mole-head');
     let randNum = Math.floor((Math.random() * 7));
+    let mole = moleHeads[randNum];
+    mole.classList.add("wgs__mole-head--hidden");
+    setTimeout(() => hideMole(mole), 1000);
 }
+
+function hideMole(mole){
+  mole.classList.remove("wgs__mole-head--hidden")
+  setTimeout(popUpRandomMole, 1000);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(popUpRandomMole, 0)
+})
